0d45e8d491976e274c4377636e30b4aa34c763b9
Sudarshan Reddy <sudarshang.reddy@gmail.com>
2023-05-04 11:22:49 +0530

    Added resource server security

--- src/main/java/com/sudarshan/catalogservice/config/SecurityConfig.java
+++ src/main/java/com/sudarshan/catalogservice/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -15,7 +17,8 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http.authorizeHttpRequests(authorize -> authorize.mvcMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
-															.anyRequest().authenticated()
+															//.anyRequest().authenticated() //Here only authentication is required
+															  .anyRequest().hasAnyRole("employee") //Here authentication and role is required
 						)
 					.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
 					.sessionManagement(sessionManagement -> sessionManagement
@@ -23,4 +26,15 @@ public class SecurityConfig {
 					.csrf(AbstractHttpConfigurer::disable) 
 					.build();
 	}
+	
+	@Bean 
+	public JwtAuthenticationConverter jwtAuthenticationConverter() { 
+		var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+		jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+
+		var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter); 
+		return jwtAuthenticationConverter; 
+	} 
 }
